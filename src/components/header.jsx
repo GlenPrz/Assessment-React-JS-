2f7aa8e55cf95fae766b22de5dccc653f420013a
GlenPrz <glenwilliam.perez36@gmail.com>
2025-04-23 09:03:29 +0800

    Initial commit of React project

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div className='head-container'>
+        <div className='head-content'>
+            <h1 className='head-title'>My Portfolio</h1>
+            <p className='head-subtitle'>Welcome to my portfolio website!</p>
+        </div>
+    </div>
+  )
+}
+
+export default header
